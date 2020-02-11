eef4d09918bda553e027917e5f232cc44aec3e29
shivausername <55323667+shivausername@users.noreply.github.com>
2020-02-11 20:32:35 +0530

    add

---
+++ precache-manifest.701dd7dc6e1831ba12721bc64d5b4324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0309178c9783d1a0183cdca08c51aa8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e7f87f4322320fd606",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f945a4365806d3debd0f",
+    "url": "/static/js/2.fa95787c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fa95787c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e7f87f4322320fd606",
+    "url": "/static/js/main.3fc0c79a.chunk.js"
+  },
+  {
+    "revision": "22df2a8fe37ba31e3fb4",
+    "url": "/static/js/runtime-main.6aea9e0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
